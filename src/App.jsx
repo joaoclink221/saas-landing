9aaba6b9c83967b3fea18c836b149a05466c182f
João Victor <ra46791342838@acaonsfatima.org.br>
2024-11-27 16:19:41 -0300

    Hero: ✔

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./sections/Features"
 import Header from "./sections/Header"
 import Hero from "./sections/Hero"
 const App = () => {
@@ -5,6 +6,7 @@ const App = () => {
     <main className="overflow-hidden">
       <Header/>
       <Hero/>
+      <Features/>
     </main>
   )
 }
